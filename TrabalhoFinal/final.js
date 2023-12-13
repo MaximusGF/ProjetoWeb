07a245af5bd0bb30d166a15b5c2102370b87d836
MaximusGF <111333702+MaximusGF@users.noreply.github.com>
2023-12-13 19:19:48 -0300

    Add files via upload

---
+++ TrabalhoFinal/final.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    document.querySelector('.c-loader').classList.add('hide');
+  }, 10000);
+
+  setTimeout(function() {
+    alert('Compra realizada com sucesso!');
+  }, 10000);
+  
